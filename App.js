7bc07e5fe3b18c538d3f769c2a1fe9f40ef74253
siddharth <jsidd06@gmail.com>
2022-01-03 15:15:46 +0530

    first commit and set state with console.log

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  const inputHandler = (text) => {
+    setText(text);
+  };
+
+  const submitHandler = () => {
+    console.log(text);
+    setText("");
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Check Your Love 💖 Percentage!</Text>
+      <Text style={styles.text}>Type Your Love Name</Text>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter Your Love Name"
+        onChangeText={inputHandler}
+      />
+      <Button onPress={submitHandler} title="Check Now" />
     </View>
   );
 }
@@ -13,8 +29,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  textInput: {
+    borderColor: "#000",
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10,
+    width: "80%",
+    borderRadius: 10,
   },
 });
